29a4080a62eaf9004ca349d4d4562d55d59355d8
Justin Fiset <justinfiset@hotmail.fr>
2023-03-08 09:34:42 -0500

    Implémentation de Pile et NoeudPile

--- src/structures/pile/NoeudPile.java
+++ src/structures/pile/NoeudPile.java
@@ -1,4 +1,31 @@
-package java.structures.pile;
+package structures.pile;
 
 public class NoeudPile {
+    private Object element;
+    private NoeudPile precedent;
+
+    public NoeudPile(Object element) {
+        this.element = element;
+    }
+
+    public NoeudPile(Object element, NoeudPile precedent) {
+        this.element = element;
+        this.precedent = precedent;
+    }
+
+    public Object getElement() {
+        return element;
+    }
+
+    public void setElement(Object element) {
+        this.element = element;
+    }
+
+    public NoeudPile getPrecedent() {
+        return precedent;
+    }
+
+    public void setPrecedent(NoeudPile precedent) {
+        this.precedent = precedent;
+    }
 }
--- src/structures/pile/Pile.java
+++ src/structures/pile/Pile.java
@@ -1,4 +1,32 @@
-package java.structures.pile;
+package structures.pile;
 
 public class Pile {
+    private int taille;
+    private NoeudPile sommet;
+
+    public Object peek() {
+        return sommet.getElement();
+    }
+
+    public int size() {
+        return taille;
+    }
+
+    public Object pop() {
+        Object r = peek();
+        sommet = sommet.getPrecedent();
+        return r;
+    }
+
+    public boolean empty() {
+        return sommet == null;
+    }
+
+    public void psuh(Object obj) {
+        if (empty()) {
+            sommet = new NoeudPile(obj);
+        } else {
+            sommet = new NoeudPile(obj, sommet);
+        }
+    }
 }
